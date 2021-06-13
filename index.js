782afb4115ec81dde6b08fb2a6eb68e1db29eeae
sam-sepi <sam.sepi.84@gmail.com>
2021-06-13 16:44:55 +0200

    api

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import router from './routes/games.js'
 
 dotenv.config()
 
 const APP = express()
 const PORT = process.env.PORT || 4000
+
+APP.use('/api/games', router)
 
 APP.listen(PORT, () => console.log('SERVER RUNNING...'))
 APP.get('/', (req, res) => res.send('HOME'))
